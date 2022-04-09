caabe60cfef5d32a15b69ef40a7ee3adc73b35d4
Alexander Pyankov <python239@mail.ru>
2022-04-10 02:06:04 +0500

    fix navigatiors, append birthday in sql-script

--- src/main/resources/db/migration/V04__insert_into_contact_table.sql
+++ src/main/resources/db/migration/V04__insert_into_contact_table.sql
@@ -1,9 +1,9 @@
-INSERT INTO testdb.contact (id, name, surname, email, phone, gender, category_id, property_id, created)
+INSERT INTO testdb.contact (id, name, surname, email, phone, gender, birthday, category_id, property_id, created)
 VALUES
-    (1,'Mary','Smith', 'mary@email.domain', '012-345-67-89', 1, 4, 12, '2022-04-08T22:10:10'),
-    (2,'Robert','Baratheon', 'king@winterfell.domain', '012-345-00-00', 0, null, null, '2022-04-08T22:10:10'),
-    (3,'Jenny','Robertson', 'jenny@email.domain', '999-345-67-77', 1, 2, 6, '2022-04-08T22:10:10'),
-    (4,'Tommy','Brown', 'tommy@email.domain', '555-444-67-00', 0, 3, 2, '2022-04-08T22:10:10'),
-    (5,'Elizabeth','Marshall', 'elizabeth@email.domain', '012-67-00-55', 1, 1, null, '2022-04-08T22:10:10'),
-    (6,'Mikky','Garret', 'mikky@email.domain', '44-345-67-33', 0, null, null, '2022-04-08T22:10:10'),
-    (7,'Richard','Stevenson', 'Richy@email.domain', '456-345-67-88', 0, 4, 8, '2022-04-08T22:10:10')
+    (1,'Mary','Smith', 'mary@email.domain', '012-345-67-89', 1, null, 4, 12, '2022-04-08T22:10:10'),
+    (2,'Robert','Baratheon', 'king@winterfell.domain', '012-345-00-00', 0,null, null, null, '2022-04-08T22:10:10'),
+    (3,'Jenny','Robertson', 'jenny@email.domain', '999-345-67-77', 1, null, 2, 6, '2022-04-08T22:10:10'),
+    (4,'Tommy','Brown', 'tommy@email.domain', '555-444-67-00', 0, '1977-01-07', 3, 2, '2022-04-08T22:10:10'),
+    (5,'Elizabeth','Marshall', 'elizabeth@email.domain', '012-67-00-55', 1, null, 1, null, '2022-04-08T22:10:10'),
+    (6,'Mikky','Garret', 'mikky@email.domain', '44-345-67-33', 0, '1990-02-03', null, null, '2022-04-08T22:10:10'),
+    (7,'Richard','Stevenson', 'Richy@email.domain', '456-345-67-88', 0, null, 4, 8, '2022-04-08T22:10:10')
